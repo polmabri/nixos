224c01b0962b11d46e3131bf9d036e8cae685479
Marek Brieger <m.brieger@appcited.com>
2025-02-21 19:10:05 +0100

    enable gc

--- configuration.nix
+++ configuration.nix
@@ -147,4 +147,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  nix.gc = {
+		automatic = true;
+		dates = "weekly";
+		options = "--delete-older-than 30d";
+	};
 }

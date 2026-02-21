0b8139973e2400e14cdb09c0e96f5fc84d7d408d
Marek Brieger <m.brieger@appcited.com>
2026-02-21 03:20:57 +0100

    refactor: disable systemd-boot editor in bootloader configuration

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,7 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.editor = false;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.tmp.useTmpfs = true;
